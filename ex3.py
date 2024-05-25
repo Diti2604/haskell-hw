7deb5a7d57500795186ccbe2316bfa8d1fee0718
Diti2604 <ditiboyboy@gmail.com>
2024-05-25 16:09:29 +0200

    small changes

--- ex3.py
+++ ex3.py
@@ -4,8 +4,11 @@ def Y(f):
 def first_n_numbers(n):
     return Y(lambda f: lambda a: 0 if a == 0 else a + f(a - 1))(n)
 
+#In case you want to test them
 # print(first_n_numbers(0)) 
 # print(first_n_numbers(1))
 # print(first_n_numbers(2))
 # print(first_n_numbers(3))
 # print(first_n_numbers(4))
+# print(first_n_numbers(13))
+# print(first_n_numbers(24))
